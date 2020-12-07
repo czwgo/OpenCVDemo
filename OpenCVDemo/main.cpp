a503002318446c2ebd4543abfadaaaa3160593c9
chenzw <zwchen2012@outlook.com>
2020-12-07 08:53:00 +0800

    添加项目文件。

---
+++ OpenCVDemo/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <opencv2/opencv.hpp>
+#include "CVMethod.h"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+	Mat src = imread("E:/2020chen/code/lena.jpg"); //  B, G, R
+	if (src.empty()) {
+		printf("could not load image....\n");
+		return -1;
+	}
+	// namedWindow("输入窗口", WINDOW_FREERATIO);
+	imshow("输入窗口", src);
+
+	CVMethod md;
+	md.blur_demo(src);
+
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
